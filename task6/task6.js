2eefa50f795f617df421557b3752fcffd9cf7f6d
TetsuhiroKAWATA <philo.cosmo717@gmail.com>
2021-05-17 15:09:57 +0900

    やっとできた・・・

--- task6/task6.js
+++ task6/task6.js
@@ -1,14 +1,14 @@
 $(function(){
 	let index = 0;
-	$('.img').eq(index).addClass('currentimg');/*imgクラスの中から、indexと同じ番号の要素を出して、currentimgと言うくらすをつけるみたいな感じかな？*/
+	$('.img').eq(index).addClass('current-img');/*imgクラスの中から、indexと同じ番号の要素を出して、currentimgと言うくらすをつけるみたいな感じかな？*/
 
 	setInterval(function(){
-		$('.img').eq(index).removeClass('currentimg');
+		$('.img').eq(index).removeClass('current-img');
 		if(index == $('.img').length - 1){
 			index = 0;
 		}else{
 			index++;
 		}
-		$('.img').eq(index).addClass('currentimg');
+		$('.img').eq(index).addClass('current-img');
 	},5000);
 });
